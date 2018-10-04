0d3216f71746021655e18c41f0e6bdcc3b6b4b51
Rhiju Das <rhiju@stanford.edu>
2018-10-04 08:44:52 -0700

    allow import of alphafold; check in a notebook

--- alphafold.py
+++ alphafold.py
@@ -99,7 +99,6 @@ def partition( sequences, circle = False ):
             Z_linear[i][j] += Z_BP[i][j]
             for k in range( i+1, i+offset):
                 if not is_cutpoint[ (k-1) % N]: Z_linear[i][j] += Z_linear[i][(k-1) % N] * Z_BP[k % N][j]
-                if i == 1 and j == 0: print 'Z_linear', Z_linear[i][j]
 
     # get the answer (in N ways!)
     Z_final = []
@@ -144,53 +143,54 @@ def partition( sequences, circle = False ):
 
 
 
-parser = argparse.ArgumentParser( description = "Compute nearest neighbor model partitition function for RNA sequence" )
-parser.add_argument( "-s","-seq","--sequences",help="RNA sequences (separate by space)",nargs='*')
-parser.add_argument("--circle", action='store_true', default=False, help='Sequence is a circle')
-args     = parser.parse_args()
-sequences = args.sequences;
-circle   = args.circle;
+if __name__=='__main__':
+    parser = argparse.ArgumentParser( description = "Compute nearest neighbor model partitition function for RNA sequence" )
+    parser.add_argument( "-s","-seq","--sequences",help="RNA sequences (separate by space)",nargs='*')
+    parser.add_argument("--circle", action='store_true', default=False, help='Sequence is a circle')
+    args     = parser.parse_args()
+    sequences = args.sequences;
+    circle   = args.circle;
 
-def output_test( Z, Z_ref = 0, bpp = [], bpp_idx= [], bpp_expected = 0):
-    print 'Z =',Z_ref,' [expected]'
-    assert( abs( (Z - Z_ref)/Z_ref )  < 1e-5 )
-    print
-    print 'bpp[0,4] = ',bpp[ bpp_idx[0] ][ bpp_idx[1] ]
-    print 'bpp[0,4] = ',bpp_expected,' [expected]'
-    assert( abs( (bpp[ bpp_idx[0] ][ bpp_idx[1] ] - bpp_expected)/bpp[ bpp_idx[0] ][ bpp_idx[1] ] )  < 1e-5 )
-    print
-
-
-if sequences == None: # run tests
-    # test of sequences where we know the final partition function.
-    sequence = 'CAAAGAA'
-    (Z, bpp) = partition( sequence, circle = True )
-    output_test( Z, C_init  * (l**7) * (1 + C_init_BP / Kd_BP ) / C_std, \
-                 bpp, [0,4], (C_init**2 * (l**3) * l_BP/ Kd_BP) / ( C_init * (l**4) + C_init**2 * (l**3) * l_BP/ Kd_BP) )
-
-    sequence = 'CAG'
-    (Z, bpp) = partition( sequence )
-    output_test( Z, 1 + C_init * l**2 / Kd_BP, \
-                 bpp, [0,2], (C_init * l**2/Kd_BP)/( 1 + C_init * l**2/Kd_BP ) )
-
-    sequences = ['C','G']
-    (Z, bpp) = partition( sequences ) # note that Z sums over only base pair (not dissociated strands!)
-    output_test( Z, C_std * l / Kd_BP/l_BP, \
-                 bpp, [0,1], 1.0 )
-
-    sequences = ['GC','GC']
-    (Z, bpp) = partition( sequences )
-    output_test( Z, (C_std/Kd_BP)*(l/l_BP)*(2 + l*l_BP*C_init/Kd_BP ), \
-                 bpp, [0,3], (1 + l*l_BP*C_init/Kd_BP )/(2 + l*l_BP*C_init/Kd_BP ) )
-
-    sequence = 'CAGGC'
-    (Z, bpp) = partition( sequence ) # note that Z sums over only base pair (not dissociated strands!)
-    output_test( Z, 1+C_init*l**2/Kd_BP * ( 2 + l ), \
-                 bpp, [0,2], C_init*l**2/Kd_BP /(  1+C_init*l**2/Kd_BP * ( 2 + l )) )
+    def output_test( Z, Z_ref = 0, bpp = [], bpp_idx= [], bpp_expected = 0):
+        print 'Z =',Z_ref,' [expected]'
+        assert( abs( (Z - Z_ref)/Z_ref )  < 1e-5 )
+        print
+        print 'bpp[0,4] = ',bpp[ bpp_idx[0] ][ bpp_idx[1] ]
+        print 'bpp[0,4] = ',bpp_expected,' [expected]'
+        assert( abs( (bpp[ bpp_idx[0] ][ bpp_idx[1] ] - bpp_expected)/bpp[ bpp_idx[0] ][ bpp_idx[1] ] )  < 1e-5 )
+        print
 
 
+    if sequences == None: # run tests
+        # test of sequences where we know the final partition function.
+        sequence = 'CAAAGAA'
+        (Z, bpp) = partition( sequence, circle = True )
+        output_test( Z, C_init  * (l**7) * (1 + C_init_BP / Kd_BP ) / C_std, \
+                     bpp, [0,4], (C_init**2 * (l**3) * l_BP/ Kd_BP) / ( C_init * (l**4) + C_init**2 * (l**3) * l_BP/ Kd_BP) )
 
-else:
-    (Z, bpp ) = partition( sequences, circle )
+        sequence = 'CAG'
+        (Z, bpp) = partition( sequence )
+        output_test( Z, 1 + C_init * l**2 / Kd_BP, \
+                     bpp, [0,2], (C_init * l**2/Kd_BP)/( 1 + C_init * l**2/Kd_BP ) )
+
+        sequences = ['C','G']
+        (Z, bpp) = partition( sequences ) # note that Z sums over only base pair (not dissociated strands!)
+        output_test( Z, C_std * l / Kd_BP/l_BP, \
+                     bpp, [0,1], 1.0 )
+
+        sequences = ['GC','GC']
+        (Z, bpp) = partition( sequences )
+        output_test( Z, (C_std/Kd_BP)*(l/l_BP)*(2 + l*l_BP*C_init/Kd_BP ), \
+                     bpp, [0,3], (1 + l*l_BP*C_init/Kd_BP )/(2 + l*l_BP*C_init/Kd_BP ) )
+
+        sequence = 'CAGGC'
+        (Z, bpp) = partition( sequence ) # note that Z sums over only base pair (not dissociated strands!)
+        output_test( Z, 1+C_init*l**2/Kd_BP * ( 2 + l ), \
+                     bpp, [0,2], C_init*l**2/Kd_BP /(  1+C_init*l**2/Kd_BP * ( 2 + l )) )
+
+
+
+    else:
+        (Z, bpp ) = partition( sequences, circle )
 
 
