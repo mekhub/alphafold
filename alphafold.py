bd5514b4941125c334e197fb7695405484533d25
Rhiju Das <rhiju@stanford.edu>
2018-10-10 17:00:36 -0700

    calculate deriv w.r.t. Kd_BP. mirabile dictu: worked on first try.

--- alphafold.py
+++ alphafold.py
@@ -2,12 +2,14 @@
 import argparse
 from util import *
 
-C_init = 1
-l    = 0.5
-l_BP = 0.1
+# Four parameter model
+C_init = 1          # a bit like a in multiloop
+l      = 0.5        # a bit like b in multiloop
+l_BP   = 0.1        # a bit like c in multiloop
+Kd_BP  = 0.001;
+
 C_init_BP = C_init * (l_BP/l) # 0.2
-Kd_BP = 0.001;
-C_std = 1; # 1 M
+C_std = 1; # 1 M. drops out in end (up to overall scale factor).
 Kd_lig = 1.0e-5 # drops out in final answer if connections/cutpoints are predefined
 min_loop_length = 1
 
@@ -20,9 +22,14 @@ def partition( sequences, circle = False ):
     # Could also use numpy arrays, but
     # eventually I'd like to use linked lists to
     # simplify backtracking.
-    C_eff = initialize_zero_matrix( N );
-    Z_BP  = initialize_zero_matrix( N );
+    C_eff    = initialize_zero_matrix( N );
+    Z_BP     = initialize_zero_matrix( N );
     Z_linear = initialize_zero_matrix( N );
+
+    # first calculate derivatives with respect to Kd_BP
+    dC_eff = initialize_zero_matrix( N );
+    dZ_BP     = initialize_zero_matrix( N );
+    dZ_linear = initialize_zero_matrix( N );
 
     # initialize
     for i in range( N ): #length of fragment
@@ -55,53 +62,96 @@ def partition( sequences, circle = False ):
                   ( any_cutpoint[i][j] or ( ((j-i-1) % N)) >= min_loop_length  ) and \
                   ( any_cutpoint[j][i] or ( ((i-j-1) % N)) >= min_loop_length  ):
                 if (not is_cutpoint[ i ]) and (not is_cutpoint[ (j-1) % N]):
-                    Z_BP[i][j] += (1.0/Kd_BP ) * ( C_eff[(i+1) % N][(j-1) % N] * l * l)
+                    Z_BP[i][j]  += (1.0/Kd_BP ) * ( C_eff[(i+1) % N][(j-1) % N] * l * l)
+                    dZ_BP[i][j] += (1.0/Kd_BP ) * ( dC_eff[(i+1) % N][(j-1) % N] * l * l)
                 for c in range( i, i+offset ):
                     if is_cutpoint[c % N]:
-                        Z_product = 1
-                        if c != i : Z_product *= Z_linear[i+1][c % N]
-                        if (c+1)%N != j:  Z_product *= Z_linear[(c+1) % N][j-1]
+                        Z_comp1 = 1
+                        Z_comp2 = 1
+                        dZ_comp1 = 0
+                        dZ_comp2 = 0
+                        if c != i :
+                            Z_comp1  = Z_linear[i+1][c % N]
+                            dZ_comp1 = dZ_linear[i+1][c % N]
+                        if (c+1)%N != j:
+                            Z_comp2 = Z_linear[(c+1) % N][j-1]
+                            dZ_comp2 = dZ_linear[(c+1) % N][j-1]
+                        Z_product  = Z_comp1 * Z_comp2
+                        dZ_product = dZ_comp1 * Z_comp2 + Z_comp1 * dZ_comp2
+
                         Z_BP[i][j] += (C_std/Kd_BP) * (l/l_BP) * Z_product
+                        dZ_BP[i][j] += (C_std/Kd_BP) * (l/l_BP) * dZ_product
+                dZ_BP[i][j] += -(1.0/Kd_BP) * Z_BP[i][j]
 
-            if not is_cutpoint[(j-1) % N]: C_eff[i][j] += C_eff[i][(j-1) % N] * l
+            if not is_cutpoint[(j-1) % N]:
+                C_eff[i][j] += C_eff[i][(j-1) % N] * l
+                dC_eff[i][j] += dC_eff[i][(j-1) % N] * l
+
             C_eff[i][j] += C_init_BP * Z_BP[i][j]
-            for k in range( i+1, i+offset):
-                if not is_cutpoint[ (k-1) % N]: C_eff[i][j] += C_eff[i][(k-1) % N] * Z_BP[k % N][j] * l_BP
+            dC_eff[i][j] += C_init_BP * dZ_BP[i][j]
 
-            if not is_cutpoint[(j-1) % N]: Z_linear[i][j] += Z_linear[i][(j - 1) % N]
-            Z_linear[i][j] += Z_BP[i][j]
             for k in range( i+1, i+offset):
-                if not is_cutpoint[ (k-1) % N]: Z_linear[i][j] += Z_linear[i][(k-1) % N] * Z_BP[k % N][j]
+                if not is_cutpoint[ (k-1) % N]:
+                    C_eff[i][j] += C_eff[i][(k-1) % N] * Z_BP[k % N][j] * l_BP
+                    dC_eff[i][j] += ( dC_eff[i][(k-1) % N] * Z_BP[k % N][j] + C_eff[i][(k-1) % N] * dZ_BP[k % N][j] ) * l_BP
+
+            if not is_cutpoint[(j-1) % N]:
+                Z_linear[i][j] += Z_linear[i][(j - 1) % N]
+                dZ_linear[i][j] += dZ_linear[i][(j - 1) % N]
+
+            Z_linear[i][j]  += Z_BP[i][j]
+            dZ_linear[i][j] += dZ_BP[i][j]
+
+            for k in range( i+1, i+offset):
+                if not is_cutpoint[ (k-1) % N]:
+                    Z_linear[i][j]  += Z_linear[i][(k-1) % N] * Z_BP[k % N][j]
+                    dZ_linear[i][j] += ( dZ_linear[i][(k-1) % N] * Z_BP[k % N][j]+ Z_linear[i][(k-1) % N] * dZ_BP[k % N][j] )
 
     # get the answer (in N ways!)
-    Z_final = []
+    Z_final  = []
+    dZ_final = []
     for i in range( N ):
         Z_final.append( 0 )
+        dZ_final.append( 0 )
+
         if not is_cutpoint[(i + N - 1) % N]:
             for c in range( i, i + N - 1):
-                if is_cutpoint[c % N]: Z_final[i] += Z_linear[i][c % N] * Z_linear[(c+1) % N][ i - 1 ] #any split segments, combined independently
+                if is_cutpoint[c % N]:
+                    #any split segments, combined independently
+                    Z_final[i]  += Z_linear[i][c % N] * Z_linear[(c+1) % N][ i - 1 ]
+                    dZ_final[i] += ( dZ_linear[i][c % N] * Z_linear[(c+1) % N][ i - 1 ] + Z_linear[i][c % N] * dZ_linear[(c+1) % N][ i - 1 ] )
 
         if is_cutpoint[(i + N - 1) % N]:
-            Z_final[i] += Z_linear[i][(i-1) % N]
+            Z_final[i]  += Z_linear[i][(i-1) % N]
+            dZ_final[i] += dZ_linear[i][(i-1) % N]
         else:
-            Z_close = C_eff[i][(i - 1) % N] * l / Kd_lig
             # Scaling Z_final by Kd_lig/C_std to match previous literature conventions
-            Z_close *= Kd_lig / C_std
-            Z_final[i] +=  Z_close
+            Z_final[i]  += C_eff[i][(i - 1) % N] * l / C_std
+            dZ_final[i] += dC_eff[i][(i - 1) % N] * l / C_std
 
     # base pair probability matrix
     bpp = initialize_zero_matrix( N );
+    bpp_tot = 0.0
     for i in range( N ):
         for j in range( N ):
             bpp[i][j] = Z_BP[i][j] * Z_BP[j][i] * Kd_BP * (l_BP / l) / Z_final[0]
+            bpp_tot += bpp[i][j]/2.0 # to avoid double counting (i,j) and (j,i)
 
     output_DP( "Z_BP", Z_BP )
     output_DP( "C_eff", C_eff, Z_final )
     output_DP( "Z_linear", Z_linear )
     output_square( "BPP", bpp );
 
+
     # stringent test that partition function is correct:
-    for i in range( N ): assert( abs( ( Z_final[i] - Z_final[0] ) / Z_final[0] ) < 1.0e-5 )
+    for i in range( N ):
+        assert( abs( ( Z_final[i] - Z_final[0] ) / Z_final[0] ) < 1.0e-5 )
+        assert( abs( ( dZ_final[i] - dZ_final[0] ) / dZ_final[0] ) < 1.0e-5 )
+
+    # calculate bpp_tot = -dlog Z_final /dlog Kd_BP in two ways! wow cool
+    bpp_tot_based_on_deriv = -dZ_final[0] * Kd_BP / Z_final[0]
+    assert( abs( ( bpp_tot - bpp_tot_based_on_deriv )/bpp_tot ) < 1.0e-5 )
+
 
     print 'sequence =', sequence
     cutpoint = ''
