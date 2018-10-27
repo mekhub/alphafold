7d54ed9309974895fad4db9068f565d68867efc5
Rhiju Das <rhiju@stanford.edu>
2018-10-27 16:57:23 -0700

    turn off modulo N in is_cutpoint --> code simplification

--- alphafold/periodicarray.py
+++ alphafold/periodicarray.py
@@ -1,10 +1,16 @@
 class PeriodicArray:
-    def __init__( self, N ):
-        self.array = [0] * N
+    '''
+    For all the various cross-checks, like equality of partition function starting at any
+     i and wrapping around to N and then back 1 and then i-1, need to keep applying modulo N.
+    '''
+    def __init__( self, N, value = 0.0 ):
+        self.array = [ value ] * N
         self.N = N
     def __getitem__( self, idx ):
         return self.array[idx % self.N]
     def __setitem__(  self, idx, item ):
         self.array[idx % self.N] = item
+    def __len__(self):
+         return len(self.array)
 
 
