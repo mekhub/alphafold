9736f241be1903b1e914d5ab375d142f6962d56f
Rhiju Das <rhiju@stanford.edu>
2018-10-29 08:55:54 -0700

    simplify model through l/l_BP scaling, to match simplification in master branch

--- alphafold.py
+++ alphafold.py
@@ -5,10 +5,8 @@ from util import *
 C_init = 1
 l    = 0.5
 l_BP = 0.1
-C_init_BP = C_init * (l_BP/l) # 0.2
 Kd_BP = 0.001;
 C_std = 1; # 1 M
-Kd_lig = 1.0e-5 # drops out in final answer if connections/cutpoints are predefined
 min_loop_length = 1
 
 def partition( sequences, circle = False ):
@@ -55,18 +53,18 @@ def partition( sequences, circle = False ):
                   ( any_cutpoint[i][j] or ( ((j-i-1) % N)) >= min_loop_length  ) and \
                   ( any_cutpoint[j][i] or ( ((i-j-1) % N)) >= min_loop_length  ):
                 if (not is_cutpoint[ i ]) and (not is_cutpoint[ (j-1) % N]):
-                    Z_BP[i][j] += (1.0/Kd_BP ) * ( C_eff[(i+1) % N][(j-1) % N] * l * l)
+                    Z_BP[i][j] += (1.0/Kd_BP ) * ( C_eff[(i+1) % N][(j-1) % N] * l * l * l_BP)
                 for c in range( i, i+offset ):
                     if is_cutpoint[c % N]:
                         Z_product = 1
                         if c != i : Z_product *= Z_linear[i+1][c % N]
                         if (c+1)%N != j:  Z_product *= Z_linear[(c+1) % N][j-1]
-                        Z_BP[i][j] += (C_std/Kd_BP) * (l/l_BP) * Z_product
+                        Z_BP[i][j] += (C_std/Kd_BP) * Z_product
 
             if not is_cutpoint[(j-1) % N]: C_eff[i][j] += C_eff[i][(j-1) % N] * l
-            C_eff[i][j] += C_init_BP * Z_BP[i][j]
+            C_eff[i][j] += C_init * Z_BP[i][j] * l_BP
             for k in range( i+1, i+offset):
-                if not is_cutpoint[ (k-1) % N]: C_eff[i][j] += C_eff[i][(k-1) % N] * Z_BP[k % N][j] * l_BP
+                if not is_cutpoint[ (k-1) % N]: C_eff[i][j] += C_eff[i][(k-1) % N] * l * Z_BP[k % N][j] * l_BP
 
             if not is_cutpoint[(j-1) % N]: Z_linear[i][j] += Z_linear[i][(j - 1) % N]
             Z_linear[i][j] += Z_BP[i][j]
@@ -84,16 +82,14 @@ def partition( sequences, circle = False ):
         if is_cutpoint[(i + N - 1) % N]:
             Z_final[i] += Z_linear[i][(i-1) % N]
         else:
-            Z_close = C_eff[i][(i - 1) % N] * l / Kd_lig
             # Scaling Z_final by Kd_lig/C_std to match previous literature conventions
-            Z_close *= Kd_lig / C_std
-            Z_final[i] +=  Z_close
+            Z_final[i] += C_eff[i][(i - 1) % N] * l / C_std
 
     # base pair probability matrix
     bpp = initialize_zero_matrix( N );
     for i in range( N ):
         for j in range( N ):
-            bpp[i][j] = Z_BP[i][j] * Z_BP[j][i] * Kd_BP * (l_BP / l) / Z_final[0]
+            bpp[i][j] = Z_BP[i][j] * Z_BP[j][i] * Kd_BP / Z_final[0]
 
     output_DP( "Z_BP", Z_BP )
     output_DP( "C_eff", C_eff, Z_final )
@@ -138,33 +134,35 @@ if __name__=='__main__':
         # test of sequences where we know the final partition function.
         sequence = 'CAAAGAA'
         (Z, bpp) = partition( sequence, circle = True )
-        output_test( Z, C_init  * (l**7) * (1 + C_init_BP / Kd_BP ) / C_std, \
-                     bpp, [0,4], (C_init**2 * (l**3) * l_BP/ Kd_BP) / ( C_init * (l**4) + C_init**2 * (l**3) * l_BP/ Kd_BP) )
+        output_test( Z, C_init  * (l**7) * (1 + (C_init * l_BP**2) / Kd_BP ) / C_std, \
+                     bpp, [0,4], (C_init * l_BP**2/ Kd_BP) / ( 1 + C_init * l_BP**2/ Kd_BP) )
 
         sequence = 'CAG'
         (Z, bpp) = partition( sequence )
-        output_test( Z, 1 + C_init * l**2 / Kd_BP, \
-                     bpp, [0,2], (C_init * l**2/Kd_BP)/( 1 + C_init * l**2/Kd_BP ) )
+        output_test( Z, 1 + C_init * l**2 * l_BP / Kd_BP, \
+                     bpp, [0,2], (C_init * l**2 * l_BP /Kd_BP)/( 1 + C_init * l**2 * l_BP/Kd_BP ) )
 
         sequences = ['C','G']
         (Z, bpp) = partition( sequences ) # note that Z sums over only base pair (not dissociated strands!)
-        output_test( Z, C_std * l / Kd_BP/l_BP, \
+        output_test( Z, C_std / Kd_BP, \
                      bpp, [0,1], 1.0 )
 
         sequences = ['GC','GC']
         (Z, bpp) = partition( sequences )
-        output_test( Z, (C_std/Kd_BP)*(l/l_BP)*(2 + l*l_BP*C_init/Kd_BP ), \
-                     bpp, [0,3], (1 + l*l_BP*C_init/Kd_BP )/(2 + l*l_BP*C_init/Kd_BP ) )
+        output_test( Z, (C_std/Kd_BP)*(2 + l**2 * l_BP**2 *C_init/Kd_BP ),
+                     bpp, [0,3], (1 + l**2 * l_BP**2 * C_init/Kd_BP )/(2 + l**2 * l_BP**2 *C_init/Kd_BP ) )
 
         sequence = 'CAGGC'
         (Z, bpp) = partition( sequence ) # note that Z sums over only base pair (not dissociated strands!)
-        output_test( Z, 1+C_init*l**2/Kd_BP * ( 2 + l ), \
-                     bpp, [0,2], C_init*l**2/Kd_BP /(  1+C_init*l**2/Kd_BP * ( 2 + l )) )
+        output_test( Z, 1 + C_init * l**2 *l_BP/Kd_BP * ( 2 + l ), \
+                 bpp, [0,2], C_init*l**2*l_BP/Kd_BP /(  1+C_init*l**2*l_BP/Kd_BP * ( 2 + l )) )
 
         sequence = 'CGACG'
         (Z, bpp) = partition( sequence ) # note that Z sums over only base pair (not dissociated strands!)
-        output_test( Z, 1 + C_init*l**2/Kd_BP + C_init*l**4/Kd_BP  + C_init * (l_BP/l) * l**4 /Kd_BP /Kd_BP , \
-                     bpp, [0,4], ( C_init*l**4/Kd_BP  + C_init * (l_BP/l) * l**4 /Kd_BP /Kd_BP ) / ( 1 + C_init*l**2/Kd_BP + C_init*l**4/Kd_BP  + C_init * (l_BP/l) * l**4 /Kd_BP /Kd_BP )  )
+        output_test( Z, 1 + C_init*l**2*l_BP/Kd_BP +
+                     C_init*l**4*l_BP/Kd_BP  +
+                     C_init**2 * (l_BP**3) * l**4 /Kd_BP /Kd_BP, \
+                 bpp, [0,4], ( C_init*l**4*l_BP/Kd_BP  + C_init**2 * (l_BP**3) * l**4 /Kd_BP /Kd_BP ) / ( 1 + C_init*l**2*l_BP/Kd_BP + C_init*l**4*l_BP/Kd_BP  + C_init**2 * (l_BP**3) * l**4 /Kd_BP /Kd_BP )  )
 
 
 
