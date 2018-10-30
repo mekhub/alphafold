f2f9a0bdd54733b5dec76a73cbfe7780feccbf78
Rhiju Das <rhiju@stanford.edu>
2018-10-30 06:47:15 -0700

    checking overhead on storing val

--- notes/test_class_speed.py
+++ notes/test_class_speed.py
@@ -4,10 +4,43 @@ import sys
 import os
 from copy import deepcopy
 sys.path.append(os.path.join(os.getcwd(), '..'))
-from alphafold.partition import DynamicProgrammingData as DP
+#from alphafold.partition import DynamicProgrammingData as DP
+
+
+###################################################################################################################33
+class DP:
+    '''
+    Dynamic programming object, with derivs and contribution accumulation.
+     X   = values (N x N)
+     dQ  = derivatives (N X N)
+     X_contrib = contributions (coming soon)
+    '''
+    def __init__( self, N ):
+        self.Q = []
+        for i in range( N ): self.Q.append( [0.0]*N )
+
+        self.dQ = deepcopy( self.Q ) # another zero matrix.
+
+        self.contrib = []
+        for i in range( N ): self.contrib.append( [[]]*N )
+
+    def __getitem__( self, idx ):
+        # overloaded []. warning: overhead! directly access object.Q[ idx ] in inner loops.
+        return self.Q[ idx ]
+
+    def __len__( self ): return len( self.Q )
+
+    def add( self, i, j, b ):
+        #  trying out a function that might make code more readable,
+        #  (could hide all contribution accumulation for backtracking -- and
+        #   perhaps even derivatives -- inside class!)
+        # but this kind of thing appears to take up too much overhead.
+        self.Q[i][j]  += b
+        self.dQ[i][j] += 0
+        self.Q_contrib[i][j].append( [i,j,b] )
 
 def getval( DP, idx ):
-    return DP.X[ idx ][ idx ]
+    return DP.Q[ idx ][ idx ]
 
 x = [[]]*500
 for i in range( 500 ): x[i] = [0.0]*500
@@ -28,9 +61,9 @@ print t1 - t0, 'y = x[56][56]'
 
 
 t0 = time.time()
-for i in range( N ): y = xDP.X[56][56]
+for i in range( N ): y = xDP.Q[56][56]
 t1 = time.time()
-print t1 - t0,'y = xDP.X[56][56]'
+print t1 - t0,'y = xDP.Q[56][56]'
 
 t0 = time.time()
 for i in range( N ): y = getval(xDP,56)
@@ -46,33 +79,33 @@ print t1 - t0,  'y = xDP[56][56]'
 # Time setting
 print 'SETTING'
 t0 = time.time()
-for i in range( N ): x[56][56] = 20
+for i in range( N ): x[56][56] = 20 * 1.5 * 1.2 * x[1][1] * x[5][5]
 t1 = time.time()
-print t1 - t0, 'x[56][56] = 20'
+print t1 - t0, 'x[56][56] = 20 * 1.5 * 1.2 * x[1][1] * x[5][5]'
 
 t0 = time.time()
-for i in range( N ): xDP.X[56][56] = 20
+for i in range( N ): xDP.Q[56][56] = 20 * 1.5 * 1.2 * x[1][1] * x[5][5]
 t1 = time.time()
-print t1 - t0,'xDP.X[56][56] = 20'
+print t1 - t0,'xDP.Q[56][56] = 20 * 1.5 * 1.2 * x[1][1] * x[5][5]'
 
 t0 = time.time()
 for i in range( N ):
-    val = 20
-    xDP.X[56][56] = val
+    val = 20  * x[1][1] * x[5][5]
+    xDP.Q[56][56] = val
 t1 = time.time()
-print t1 - t0,'val = 20; xDP.X[56][56] = val'
+print t1 - t0,'val = 20 * 1.5 * 1.2 * x[1][1] * x[5][5]; xDP.Q[56][56] = val'
 
 t0 = time.time()
-for i in range( N ): xDP[56][56] = 20
+for i in range( N ): xDP[56][56] = 20 * 1.5 * 1.2 * x[1][1] * x[5][5]
 t1 = time.time()
-print t1 - t0,'xDP[56][56] = 20'
+print t1 - t0,'xDP[56][56] = 20 * 1.5 * 1.2 * x[1][1] * x[5][5]'
 
 
 # Time setting, including derivs
 print 'SETTING INCLUDE DERIVS'
 t0 = time.time()
 for i in range( N ):
-    x[56][56] = 20
+    x[56][56] = 20 * x[1][1] * x[5][5]
     dx[56][56] = 0
 t1 = time.time()
 print t1 - t0, 'x[56][56] = 20, dx[56][56] = 20'
@@ -85,10 +118,10 @@ print t1 - t0, 'x[56][56] = (20,0)'
 
 t0 = time.time()
 for i in range( N ):
-    xDP.X[56][56] = 20
-    xDP.dX[56][56] = 0
+    xDP.Q[56][56] = 20
+    xDP.dQ[56][56] = 0
 t1 = time.time()
-print t1 - t0,'xDP.X[56][56] = 20,  xDP.dX[56][56]'
+print t1 - t0,'xDP.Q[56][56] = 20,  xDP.dQ[56][56]'
 
 t0 = time.time()
 for i in range( N ):
@@ -110,11 +143,11 @@ print t1 - t0, 'x[56][56] = 20'
 
 t0 = time.time()
 for i in range( N ):
-    xDP.X[56][56] = 20
-    xDP.dX[56][56] = 0
-    xDP.X_contrib[56][56].append( [x,56,56,20] )
+    xDP.Q[56][56] = 20
+    xDP.dQ[56][56] = 0
+    xDP.Q_contrib[56][56].append( [x,56,56,20] )
 t1 = time.time()
-print t1 - t0,'xDP.X[56][56] = 20'
+print t1 - t0,'xDP.Q[56][56] = 20'
 
 t0 = time.time()
 for i in range( N ):
