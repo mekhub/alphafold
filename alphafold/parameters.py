67446fc025c703ccf0e73959f70019126c2bfddc
Rhiju Das <rhiju@stanford.edu>
2018-11-18 17:01:41 -0800

    new parameters v1.6 preserves tRNA and better P4-P6 (but cannot make both work within current model)

--- alphafold/parameters.py
+++ alphafold/parameters.py
@@ -1,6 +1,7 @@
 import math
 from base_pair_types import BasePairType
 from util.constants import KT_IN_KCAL
+from old_parameters import *
 
 class AlphaFoldParams:
     '''
@@ -32,15 +33,19 @@ class AlphaFoldParams:
 
 def get_params( params = None, suppress_all_output = False ):
     params_object = None
-    if params == None or params =='': params_object = get_latest_params()
+
+    if isinstance(params,AlphaFoldParams): return params
+    elif params == None or params =='': params_object = get_latest_params()
     elif params == 'minimal':         params_object = get_minimal_params()
-    elif isinstance(params,AlphaFoldParams): return params
+    elif params == 'v0.1':  params_object = get_params_v0_1( AlphaFoldParams()  )
+    elif params == 'v0.15': params_object = get_params_v0_15( AlphaFoldParams() )
+    elif params == 'v0.16': params_object = get_params_v0_16( AlphaFoldParams() )
     else: print 'unrecognized params requested: ', params
     if not suppress_all_output: print 'Parameters: ', params_object.name, ' version', params_object.version
     return params_object
 
 def get_latest_params():
-    return get_params_v0_15()
+    return get_params_v0_16( AlphaFoldParams() )
 
 def get_minimal_params():
     params = AlphaFoldParams()
@@ -65,23 +70,23 @@ def get_minimal_params():
 
     return params
 
-def get_params_v0_15():
+def get_params_v0_16( params ):
     # Starting to make use of train_alphafold.py on tRNA and P4-P6.
-    params = AlphaFoldParams()
     params.name     = 'alphafold'
-    params.version  = '0.15'
+    params.version  = '0.16'
 
     params.min_loop_length = 3
 
     # Seven parameter model
     dG_init = +4.09 # Turner 1999, kcal/mol
-    Kd_BP_CG = 1.0 * math.exp( dG_init/ KT_IN_KCAL)
+    Kd_BP_CG = 1.0 * math.exp( dG_init/ KT_IN_KCAL) # 762 M
+    Kd_BP_AU = 10.0**5.0 # 100000 M
+    Kd_BP_GU = 10.0**5.0 # 100000 M
+
     dG_terminal_AU = 0.5 # Turner 1999, kcal/mol -- NUPACK
-    Kd_BP_AU = 10.0**5.40731537 # 255000
-    Kd_BP_GU = 10.0**3.5863221 # 4000
 
     dG_CG_CG = -3.30 # Turner 1999 5'-CC-3'/5'-GG-3', kcal/mol
-    params.C_eff_stacked_pair = math.exp( -dG_CG_CG / KT_IN_KCAL ) * Kd_BP_CG
+    params.C_eff_stacked_pair = 10**5.425 # about 10^5
 
     # From nupack rna1999.params
     #>Multiloop terms: ALPHA_1, ALPHA_2, ALPHA_3
@@ -115,53 +120,5 @@ def get_params_v0_15():
     # turn off coax!?
     params.K_coax = 0.0
     params.l_coax = 1.0
-
     return params
 
-def get_params_v0_1():
-    params = AlphaFoldParams()
-    params.name     = 'alphafold'
-    params.version  = '0.1'
-
-    params.min_loop_length = 3
-
-    # Seven parameter model
-    dG_init = +4.09 # Turner 1999, kcal/mol
-    Kd_BP_CG = 1.0 * math.exp( dG_init/ KT_IN_KCAL)
-    dG_terminal_AU = 0.5 # Turner 1999, kcal/mol -- NUPACK
-    Kd_BP_AU = Kd_BP_CG *math.exp( dG_terminal_AU/ KT_IN_KCAL )
-    Kd_BP_GU = Kd_BP_AU * 10 # fudge factor to make GU weaker.
-
-    dG_CG_CG = -3.30 # Turner 1999 5'-CC-3'/5'-GG-3', kcal/mol
-    params.C_eff_stacked_pair = math.exp( -dG_CG_CG / KT_IN_KCAL ) * Kd_BP_CG
-
-    # From nupack rna1999.params
-    #>Multiloop terms: ALPHA_1, ALPHA_2, ALPHA_3
-    #>ML penalty = ALPHA_1 + s * ALPHA_2 + u *ALPHA_3
-    #>s = # stems adjacent to ML, u = unpaired bases in ML
-    # 340   40    0
-    #>AT_PENALTY:
-    #>Penalty for non GC pairs that terminate a helix
-    #  50
-    dG_bulge = 3.4 # bulge cost is roughly 3-4 kcal/mol
-    dG_multiloop_stems = 0.40 # in kcal/mol
-    dG_multiloop_unpaired = 0.0 #0.40 # in kcal/mol -- ZERO in NUPACK -- fudging here.
-    params.C_init = 1.0 * math.exp( -dG_bulge / KT_IN_KCAL )
-    params.l = math.exp( dG_multiloop_unpaired / KT_IN_KCAL )
-    params.l_BP = math.exp( dG_multiloop_stems/KT_IN_KCAL ) / params.l
-
-    base_pair_types = []
-    base_pair_types.append( BasePairType( 'C', 'G', Kd_BP_CG ) )
-    base_pair_types.append( BasePairType( 'A', 'U', Kd_BP_AU ) )
-    base_pair_types.append( BasePairType( 'G', 'U', Kd_BP_GU ) )
-
-    #Kd_BP_GA = Kd_BP_AU*100000  # fudge factor to make GA weaker.
-    #base_pair_types.append( BasePairType( 'G', 'A', Kd_BP_GA ) ) # totally made up
-    #Kd_BP_AA = Kd_BP_AU * 40 # fudge factor to make GU weaker.
-    #base_pair_types.append( BasePairType( 'A', 'A', Kd_BP_AA ) ) # totally made up
-
-    params.base_pair_types = base_pair_types
-    params.K_coax = 10
-    params.l_coax = 1
-
-    return params
--- train_alphafold.py
+++ train_alphafold.py
@@ -5,22 +5,23 @@ from alphafold.partition import partition
 from alphafold.score_structure import score_structure
 import numpy as np
 
-def free_energy_gap( x, sequences, structures ):
+def free_energy_gap( x, sequence_structure_pairs ):
     dG_gap = 0.0
     params = get_params( suppress_all_output = True )
     params.K_coax = 0.0
     (params.C_eff_stacked_pair, params.C_init, params.base_pair_types[1].Kd_BP, params.base_pair_types[2].Kd_BP ) = 10.0**x
 
-    for sequence,structure in zip( sequences, structures ):
+    for sequence,structure in sequence_structure_pairs:
         p = partition( sequence, params = params, suppress_all_output = True, mfe = True )
         dG = p.dG
         dG_structure = score_structure( sequence, structure, params = params )
         dG_gap += dG_structure - dG # will be a positive number, best case zero.
-    print p.struct_MFE, x, dG_gap
+        print p.struct_MFE, x, dG_gap
+    print
     return dG_gap
 
-#sequence  = 'GCGGAUUUAGCUCAGUUGGGAGAGCGCCAGACUGAAGAUCUGGAGGUCCUGUGUUCGAUCCACAGAAUUCGCACCA'
-#structure = '(((((((..((((........)))).(((((.......))))).....(((((.......))))))))))))....'
+tRNA_sequence  = 'GCGGAUUUAGCUCAGUUGGGAGAGCGCCAGACUGAAGAUCUGGAGGUCCUGUGUUCGAUCCACAGAAUUCGCACCA'
+tRNA_structure = '(((((((..((((........)))).(((((.......))))).....(((((.......))))))))))))....'
 
 # P4-P6
 #sequence = 'GGAAUUGCGGGAAAGGGGUCAACAGCCGUUCAGUACCAAGUCUCAGGGGAAACUUUGAGAUGGCCUUGCAAAGGGUAUGGUAAUAAGCUGACGGACAUGGUCCUAACCACGCAGCCAAGUCCUAAGUCAACAGAUCUUCUGUUGAUAUGGAUGCAGUUCA'
@@ -35,14 +36,13 @@ sequence  = ['GGAAUUGCGGGAAAGG','CUAACCACGCAGCCAAGUCCUAAGUC','GAUAUGGAUGCAGUUCA'
 #structure =  '.....((((((...(())..)).))))((...((((...((()))..))))...))...'
 
 # P4-P6 outer junction -- easier to read input
-sequence  = 'GGAAUUGCGGGAAAGG CUAACCACGCAGCCAAGUCCUAAGUC GAUAUGGAUGCAGUUCA'
-structure = '.....((((((...(( ))..)).))))((...((((...((( )))..))))...))...'
+P4P6_outerjunction_sequence  = 'GGAAUUGCGGGAAAGG CUAACCACGCAGCCAAGUCCUAAGUC GAUAUGGAUGCAGUUCA'
+P4P6_outerjunction_structure = '.....((((((...(( ))..)).))))((...((((...((( )))..))))...))...'
 
 
-sequences  = [sequence]
-structures = [structure]
+sequence_structure_pairs  = [ (tRNA_sequence , tRNA_structure), (P4P6_outerjunction_sequence, P4P6_outerjunction_structure) ]
 
-loss = lambda x, sequences = sequences, structures = structures : free_energy_gap( x, sequences, structures )
+loss = lambda x, sequence_structure_pairs = sequence_structure_pairs : free_energy_gap( x, sequence_structure_pairs )
 
 # simple 1-D scan
 #for x0 in range(10):  loss( [x0] )
