8fd57954b92611cbd980e85fd50e47b54a3fa916
Rhiju Das <rhiju@stanford.edu>
2018-11-19 12:18:23 -0800

    in train_alphafold, package different parameter set optimizations into apply_params func

--- train_alphafold.py
+++ train_alphafold.py
@@ -5,24 +5,11 @@ from alphafold.partition import partition
 from alphafold.score_structure import score_structure
 import numpy as np
 
-def free_energy_gap( x, sequence_structure_pairs ):
+def free_energy_gap( x, sequence_structure_pairs, apply_params ):
     dG_gap = 0.0
     params = get_params( suppress_all_output = True )
     params.K_coax = 0.0
-
-    q = 10.0**x
-    params.C_eff_stacked_pair = q[0]
-    params.initialize_C_eff_stack()
-    params.C_init = q[1]
-    params.base_pair_types[2].Kd_BP = q[2]
-    params.base_pair_types[3].Kd_BP = q[2] # A-U
-    params.base_pair_types[4].Kd_BP = q[3]
-    params.base_pair_types[5].Kd_BP = q[3] # G-U
-    #for bpt in params.base_pair_types: print bpt.nt1, bpt.nt2, bpt.Kd_BP, q
-    #for bpt1 in params.base_pair_types:
-    #    for bpt2 in params.base_pair_types:
-    #        print bpt1, bpt2, params.C_eff_stack[ bpt1][bpt2]
-
+    apply_params( params, x )
     for sequence,structure in sequence_structure_pairs:
         p = partition( sequence, params = params, suppress_all_output = True, mfe = True )
         dG = p.dG
@@ -54,13 +41,23 @@ P4P6_outerjunction_structure = '.....((((((...(( ))..)).))))((...((((...((( ))).
 
 sequence_structure_pairs  = [ (tRNA_sequence , tRNA_structure), (P4P6_outerjunction_sequence, P4P6_outerjunction_structure) ]
 
-loss = lambda x, sequence_structure_pairs = sequence_structure_pairs : free_energy_gap( x, sequence_structure_pairs )
+def apply_params_Ceff_Cinit_KdAU_KdGU( params, x ):
+    q = 10.0**x
+    params.C_eff_stacked_pair = q[0]
+    params.initialize_C_eff_stack()
+    params.C_init = q[1]
+    params.base_pair_types[2].Kd_BP = q[2]
+    params.base_pair_types[3].Kd_BP = q[2] # A-U
+    params.base_pair_types[4].Kd_BP = q[3]
+    params.base_pair_types[5].Kd_BP = q[3] # G-U
+
+x0 = np.array( [5, 1, 3, 3] )
+apply_params_func = apply_params_Ceff_Cinit_KdAU_KdGU
+
+loss = lambda x : free_energy_gap( x, sequence_structure_pairs, apply_params_func )
 
 # simple 1-D scan
 #for x0 in range(10):  loss( [x0] )
-
-x0 = np.array( [5, 1, 3, 3] )
-
 
 #result = minimize( loss, x0, method = 'Nelder-Mead' )
 result = minimize( loss, x0, method = 'L-BFGS-B' )
